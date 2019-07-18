c3b583960a89e40160f4f3093fa6d4203dc0d1a0
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-17 18:46:42 -0700

    updated for docker

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 const houseSchema = require("../zeelo-database/houseSchema.js");
+const cors = require('cors')
 
+
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../react-client/dist"));
