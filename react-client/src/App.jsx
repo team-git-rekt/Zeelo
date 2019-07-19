0a41d757220ef580aadebc41045f0acd3d071389
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-19 11:13:56 -0700

    updated - bug in proxy

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import Overview from "./components/Overview.jsx";
-import Dropdown from "./components/Dropdown.jsx";
-import Pricing from "./components/Pricing.jsx";
-
+import React from "react"
+import Overview from "./components/Overview.jsx"
+import Dropdown from "./components/Dropdown.jsx"
+import Pricing from "./components/Pricing.jsx"
 const App = () => {
   return (
     <div>
